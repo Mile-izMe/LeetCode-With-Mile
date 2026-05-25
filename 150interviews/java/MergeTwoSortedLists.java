2c7b1d2e74bfc89a885cc7dff50dcd295d8617c0
MiLe <leducminhdhqt@gmail.com>
2026-05-25 13:48:14 +0700

    Refactor ListNode class visibility and remove unused constructor in MergeTwoSortedLists

--- 150interviews/java/MergeTwoSortedLists.java
+++ 150interviews/java/MergeTwoSortedLists.java
@@ -10,12 +10,10 @@
  */
 
 public class MergeTwoSortedLists {
-    public class ListNode {
+    private class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
         ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
