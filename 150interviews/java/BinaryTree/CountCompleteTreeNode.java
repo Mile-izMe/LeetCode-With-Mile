e1ab1e7faab7f32631d001aed0400c76a458212a
MiLe <leducminhdhqt@gmail.com>
2026-05-28 16:26:16 +0700

    Add CountCompleteTreeNode class to count the number of nodes in a complete binary tree

---
+++ 150interviews/java/BinaryTree/CountCompleteTreeNode.java
@@ -0,0 +1,11 @@
+package BinaryTree;
+
+public class CountCompleteTreeNode {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
